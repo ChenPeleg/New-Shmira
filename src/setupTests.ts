c8b861c0449aa76e9eab3a21febefb4bfd58fcca
chenp <chenpeleg@gmail.com>
2022-09-03 22:25:20 +0300

    runs ok need to replace enzym

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,10 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import React from 'react';
+//import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+//import {configure} from 'enzyme';
+
+
+React.useLayoutEffect = React.useEffect
+//configure({adapter: new Adapter()})
