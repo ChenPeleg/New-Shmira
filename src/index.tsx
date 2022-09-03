932bdb3bc56b08ee9478dade7e36d17c95fd06db
chenp <chenpeleg@gmail.com>
2022-09-03 21:52:13 +0300

    installed mui

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+import { Root } from './hoc/Root';
+import App from './hoc/App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Root>
+      <App />
+    </Root>,
+
   </React.StrictMode>
 );
 
